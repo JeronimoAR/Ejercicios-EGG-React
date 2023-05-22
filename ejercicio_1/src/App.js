c7e24f62eb15a83818d53d157e3ce3ad010a938e
Jeronimo Arias <jeronimoarias1212@gmail.com>
2023-05-22 17:23:11 -0500

    Ejercicio_1

---
+++ ejercicio_1/src/App.js
@@ -0,0 +1,13 @@
+import Ejemplo from "./pages/ejemplo";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Ejemplo />
+      <h1></h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ ejercicio_1/src/pages/ejemplo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Ejemplo() {
+    const titulo = "Harry Potter"
+    const subtitulo = "La Piedra Filosofal"
+  return (
+    <div>
+      <h1>{titulo}</h1>
+      <h2>{subtitulo}</h2>
+    </div>
+  )
+}
